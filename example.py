148ceaa8190a3bfaa1fc309aed1a3371f9ceffd2
RealDealLea1 <chris.leal@outlook.com>
2018-09-20 09:22:44 -0700

    Update example.py

--- example.py
+++ example.py
@@ -7,5 +7,5 @@ for day in WEEK_ARRAY:
 		print("Hello dev project team. Welcome to today's meeting.")
 
 s_meeting=""
-if(S_meeting == "finished"):
+if(s_meeting == "finished"):
 	print("Make sure to come to our study room 1-4 on M/W.")
