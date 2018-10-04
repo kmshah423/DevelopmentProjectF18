7bde498943433ad2984a101dee7db94b7f2ce659
Willy Paredes <wsparedes@cpp.edu>
2018-10-04 13:00:31 -0700

    Add files via upload

---
+++ Fizz Buzz/Fizz_Buzz.py
@@ -0,0 +1,11 @@
+
+
+for i in range(1, 100):
+    if i % 15 == 0:
+        print("Fizz Buzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
