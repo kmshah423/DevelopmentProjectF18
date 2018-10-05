aabb8223b311e46ad2835e152ba50635395351c8
Ceasar Navarro <c.navarro.9556@gmail.com>
2018-10-05 00:05:56 -0700

    Bonus

--- Navarro, Ceasar/Project1.py
+++ Navarro, Ceasar/Project1.py
@@ -1,9 +1,24 @@
-for i in range(100):
-    if(i % 3 == 0):
-        print("Fizz") 
+"""for i in range(1,100):
+    if(i%3 == 0 and i%5 == 0):
+        print("Fizz Buzz") 
     elif(i % 5 == 0):
-        print("Buzz")
-    elif(i % 3 == 0 and i % 5 == 0):
-        print("Fizz Buzz")
+     print("Buzz")
+    elif(i % 3 == 0):
+        print("Fizz")
     else:
-        print(i)
+         print(i)"""
+
+def bonus(n=1, m=100):
+    if(n > m):
+        return 
+    elif(n%3 ==0 and n%5 == 0):
+        print("Fizz Buzz")
+    elif(n%5 == 0):
+        print("Buzz")
+    elif(n%3 == 0):
+        print("Fizz")
+    else:
+        print(n)
+    bonus(n + 1, m)
+if __name__ == '__main__':
+   bonus()
