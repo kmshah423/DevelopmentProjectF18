48d964282279a4e5757dc72e1149c3961bb4bb04
Ceasar Navarro <c.navarro.9556@gmail.com>
2018-10-05 10:49:37 -0700

    almost done

--- Navarro, Ceasar/Project3.py
+++ Navarro, Ceasar/Project3.py
@@ -1,30 +1,40 @@
 class Group_16:
 
     def __init__(self):
-        self.time = []
-        self.days = []
+        self.time = ""
+        self.days = ""
         self.names = ["Eric", "Michael", "Ceasar"]
 
-    def meet_up(self,t,d):
-        self.time.append(t)
-        self.days.append(d)
+    def Meet_up(self,t,d):
+        print("Can meet" + str(self.time) + str(self.days))
 
-    def print_Members(self):
+    def Print_Members(self):
         print(self.names)
 
 
 
 
 class GroupMembers:
-    def __init__(self,name,major,classSchdule[])
+    def __init__(self,name,major,classSchdule):
+        name = ""
+        major = 0
+        classSchdule = classSchdule.append[classSchdule]
         self.name = name
         self.major = major
-        self.classSchdule[]
+        self.classSchdule = classSchdule
 
+    
+    def PrintClassInfo(self):
+        x = len(self.classSchdule)
+        for i in range(x):
+            print(self.classSchdule[i])
 
-
-        
-
-
-
-     
+class Class:
+    def __init__(self,department ,classNumber,startTime,endTime):
+        self.department = department
+        self.classNumber = classNumber
+        self.startTime = startTime
+        self.endTime = endTime
+    
+    def PrintClassInfo(self):
+        print("h")
