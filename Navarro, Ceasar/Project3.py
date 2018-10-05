9892ddad5945fcd384b63dfd3f3f0b83130b8abe
Ceasar Navarro <c.navarro.9556@gmail.com>
2018-10-05 00:37:01 -0700

    working .... kiidna

---
+++ Navarro, Ceasar/Project3.py
@@ -0,0 +1,23 @@
+class Group_16:
+
+    def __init__(self):
+        self.time = []
+        self.days = []
+        self.names = ["Eric", "Michael", "Ceasar"]
+
+    def meet_up(self,t,d):
+        self.time.append(t)
+        self.days.append(d)
+
+    def print_Members(self):
+        print(self.names)
+
+
+newGroup = Group_16()
+
+newGroup.print_Members()
+        
+
+
+
+     
