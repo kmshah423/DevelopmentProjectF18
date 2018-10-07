61e3d5e6f7df036177a7d910918fd7adc5934310
42flyingfish <cire.ttalp@gmail.com>
2018-10-06 23:30:08 -0700

    Added my barebone instance of project 3

---
+++ Platt,Eric/proj3.py
@@ -0,0 +1,24 @@
+class Group_16:
+    def __init__(self, members):
+        self.members = members
+
+    def meet_up(self, day, time):
+        pass
+
+    def print_members(self):
+        pass
+
+
+class GroupMember:
+    def __init__(self, name, major, classSchedule):
+        self.name = name
+        self.major = major
+        self.classSchedule = classSchedule
+
+    def print_schedule(self):
+        pass
+
+
+class Class:
+    def __init__(self, department, classNumber, startTime, endTime):
+        pass
